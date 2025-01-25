0d3d2ae7f55e8d0c8347aa57563835b04b31a735
Walid Hassan <swordwalid@gmail.com>
2025-01-25 22:30:38 +0600

    Remove pollapp from urls

--- django_target/urls.py
+++ django_target/urls.py
@@ -19,7 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("polls/",include('pollapp.urls')),
     path('locallib/',include('LocalLibrary.urls')),
     path('accounts/',include('django.contrib.auth.urls')),
 ]
