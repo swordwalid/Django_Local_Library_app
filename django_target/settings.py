7160a7e91ab502883399b6a99e78805904c1accb
Walid Hassan <swordwalid@gmail.com>
2025-01-26 14:48:38 +0600

    Added Allowed hosts and CSRF trusted origins

--- django_target/settings.py
+++ django_target/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY=os.environ.get('DJANGO_SECRET_KEY','django-insecure-)wnll@8)em%o_az2=
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['swordwalid.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
@@ -157,3 +157,9 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+## For example, for a site URL is at 'web-production-3640.up.railway.app'
+## (replace the string below with your own site URL):
+CSRF_TRUSTED_ORIGINS = ['https://swordwalid.pythonanywhere.com']
+
+# During development/for this tutorial you can instead set just the base URL
+# CSRF_TRUSTED_ORIGINS = ['https://*.pythonanywhere.com']
